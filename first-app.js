2e0c5b14b2feb7aa86c2c3bb9fabd8dc8c0fe6df
anuragsrma <anuragsharma0140@gmail.com>
2023-12-07 22:36:35 +0530

    Lets Revise Javascript concepts

--- first-app.js
+++ first-app.js
@@ -1,1 +1,14 @@
-console.log('Hello world from node js');
+//console.log('Hello world from node js');
+
+const product = (a,b) =>  a *b;
+console.log(product(3,2));
+
+const student = {
+    name : "Anurag",
+    age : 25,
+    greet(){
+       console.log('hi, I am ' + this.name);
+    }
+};
+
+student.greet();
