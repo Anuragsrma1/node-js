95e2a9920581988e6613389df1869b98a2d6655e
anuragsrma <anuragsharma0140@gmail.com>
2023-12-10 16:29:44 +0530

    Request / Response

--- creating server/app.js
+++ creating server/app.js
@@ -1,7 +1,26 @@
 const http = require('http');
 
 const server = http.createServer((req,res) => {
-   console.log(req);
-  console.log("Anurag");
+   console.log(req.url , req.method , req.headers);
+ // console.log("Anurag");
+  res.setHeader('Content-Type', 'home/html');
+  res.write('<html>');
+  res.write('<head><title>My first Page </title></head>');
+ res.write('<body><h1>Welcome home </h1></body>');
+ res.write('<html>');
+
+ res.setHeader('Content-Type', 'about/html');
+ res.write('<html>');
+ res.write('<head><title>My first Page </title></head>');
+res.write('<body><h1>Welcome to About Us page</h1></body>');
+res.write('<html>');
+
+res.setHeader('Content-Type', 'node/html');
+res.write('<html>');
+res.write('<head><title>My first Page </title></head>');
+res.write('<body><h1>Welcome to my Node Js project</h1></body>');
+res.write('<html>');
+
 });
-server.listen(4000);
+
+server.listen(3000);
