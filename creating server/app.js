66022c859509bf1503df4b073921ac91973bed66
anuragsrma <anuragsharma0140@gmail.com>
2023-12-12 15:07:41 +0530

    Writing and reading from a file

--- creating server/app.js
+++ creating server/app.js
@@ -1,25 +1,58 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req,res) => {
-   console.log(req.url , req.method , req.headers);
- // console.log("Anurag");
-  res.setHeader('Content-Type', 'home/html');
-  res.write('<html>');
-  res.write('<head><title>My first Page </title></head>');
- res.write('<body><h1>Welcome home </h1></body>');
- res.write('<html>');
+ 
+const url = req.url;
+const method = req.method;
+const body = [];
 
- res.setHeader('Content-Type', 'about/html');
- res.write('<html>');
- res.write('<head><title>My first Page </title></head>');
-res.write('<body><h1>Welcome to About Us page</h1></body>');
-res.write('<html>');
+if(url === '/'){
 
-res.setHeader('Content-Type', 'node/html');
-res.write('<html>');
-res.write('<head><title>My first Page </title></head>');
-res.write('<body><h1>Welcome to my Node Js project</h1></body>');
-res.write('<html>');
+  fs.readFile('message.txt' , {encoding : "utf-8"} , (err,Data) =>{
+    if(err){
+      console.log(err);
+    }
+    console.log(`data from file` + Data);
+    res.write('<html');
+    res.write('<head><title>Enter message</title></head>');
+    res.write(`<body>${Data}</body>`);
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+  
+ 
+  res.write('<html>');  
+  
+  return res.end();
+});
+ }else if(url === '/message' && method === 'POST'){
+
+   req.on('data', (chunk) =>{
+    console.log(chunk);
+    body.push(chunk);
+   });
+   return req.on('end', () => {
+          const parsedBody = Buffer.concat(body).toString();
+          const message = parsedBody.split('=')[1];
+          fs.writeFile('message.txt', message , (err) => {
+            if(err){
+              console.log(err);
+            }
+
+             console.log('indise fs.writefile');
+            res.statusCode = 302;
+            res.setHeader('Location','/');
+            return res.end();
+          }); 
+    });
+  }
+ else{
+     res.setHeader('Content-Type', 'text/html');
+     res.write('<html>');
+     res.write('<head><title>My first Page </title></head>');
+     res.write('<body><h1>Welcome home </h1></body>');
+     res.write('<html>');
+
+  }
 
 });
 
